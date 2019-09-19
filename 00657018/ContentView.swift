419abc07833206314a443c390b977bf55d060a75
HsiehYuHeng <k28998989@gmail.com>
2019-09-19 17:04:51 +0800

    Initial Commit

---
+++ 00657018/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00657018
+//
+//  Created by User17 on 2019/9/19.
+//  Copyright © 2019 00557021. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
